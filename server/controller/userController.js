598c5ebe8a6d38a263accb743cb6d7a9da76197d
AASIYO MAXMED APDI <aasiyomaxmedapdi@gmail.com>
2024-12-01 21:40:56 -0800

    chtgt

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -3,88 +3,64 @@
 import { Webhook } from "svix";
 import User from "../model/userModel.js";
 
-export const  clerkWebhook = async (req, res) => {
+export const clerkWebhook = async (req, res) => {
+  try {
+    const webhook = new Webhook("whsec_UnZk3ssoXdYqJmxJ8FK49SSbdM85VDNT");
 
-  
-    try {
+    const payload = JSON.stringify(req.body);
+    const headers = {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
+    };
 
+    // Verify the webhook
+    webhook.verify(payload, headers);
 
+    const { data, type } = req.body;
 
+    switch (type) {
+      case "user.created": {
+        const userData = {
+          clerkId: data.id,
+          email: data.email_addresses[0].email_address,
+          photo: data.image_url,
+          firstName: data.first_name,
+          lastName: data.last_name,
+          creditBalance: data.creditBalance || 0,
+        };
 
+        await User.create(userData);
+        console.log("User created on " + new Date());
+        return res.json({});
+      }
 
-        const webhook = new Webhook(
-            "whsec_UnZk3ssoXdYqJmxJ8FK49SSbdM85VDNT"
-        );
-            
+      case "user.updated": {
+        const userData = {
+          email: data.email_addresses[0].email_address,
+          photo: data.image_url,
+          firstName: data.first_name,
+          lastName: data.last_name,
+          creditBalance: data.creditBalance || 0,
+        };
 
-        webhook.verify(JSON.stringify(req.body) ,{
-            headers: req.headers,
-             "svix-id" : req.headers['svix-id'],
-             'svix-timestamp' :  req.headers['svix-timestamp'],
-             'svix-signature' :  req.headers['svix-signature'],
+        await User.findOneAndUpdate({ clerkId: data.id }, userData);
+        console.log("User updated on " + new Date());
+        return res.json({});
+      }
 
-        });
-       
+      case "user.deleted": {
+        await User.findOneAndDelete({ clerkId: data.id });
+        console.log("User deleted on " + new Date());
+        return res.json({});
+      }
 
-
-        const { date , type } = req.body;
-
-
-        switch (type) {
-            case "user.created": {
-                const userData = {
-                    clerkId: data.id,
-                    email: data.email_addresses[0],
-                    photo: data.image_url,
-                    firstName: data.first_name,
-                    lastName: data.last_name,
-                    creditBalance: data.creditBalance
-                }
-
-                await User.create(userData)
-                res.json({})
-                console.log("User created on " + date);
-                break;
-            }
-                
-                case "user.updated": {
-
-                    const userData = {
-                        clerkId: data.id,
-                        email: data.email_addresses[0],
-                        photo: data.image_url,
-                        firstName: data.first_name,
-                        lastName: data.last_name,
-                        creditBalance: data.creditBalance
-                    }
-    
-
-                    await  User.findOneAndUpdate({clerkId: data.id}, userData)
-
-                    res.json({})
-
-                console.log("User updated on " + date);
-                break;
-            }
-
-
-            case "user.deleted": {
-
-                await User.findOneAndDelete({clerk : date.id})
-
-                console.log("User deleted on " + date);
-                break;
-            }
-        
-            default:
-                break;
-        }
-
-
-        console.log("Webhook verified");
-
-    }  catch(e){
-        console,log(e , "errror in webhook")
-        return res.status(400).json({message: "Webhook not verified" ,success : false})
+      default:
+        console.log("Unhandled webhook type: " + type);
+        return res.status(200).json({ message: "Unhandled webhook type" });
     }
-}
+  } catch (e) {
+    console.log(e, "Error in webhook");
+    return res.status(400).json({ message: "Webhook not verified", success: false });
+  }
+};
